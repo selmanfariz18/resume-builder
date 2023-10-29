5869cab7b5c08a53697a85e20d83d42fa866e2e5
shebreen <96862340+shebreen@users.noreply.github.com>
2023-10-29 16:43:46 +0530

    Update scripts.js

--- build/scripts.js
+++ build/scripts.js
@@ -1,62 +1,56 @@
-
-
-const templateLinks = document.querySelectorAll('.select-template-link');
+const selectTemplateLinks = document.querySelectorAll('.select-template-link');
 const resumeEditor = document.getElementById('resume-editor');
 const generatedResume = document.getElementById('generated-resume');
-const resumeForm = document.getElementById('resume-form');
-const downloadButton = document.getElementById('download-resume');
-const previewResume = document.getElementById('preview-resume');
 
-
-let userData = {
-    name: '',
-    email: '',
-    phone: '',
-
-    
-};
-
-
-templateLinks.forEach((link) => {
+selectTemplateLinks.forEach((link) => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
-        
         document.getElementById('template-selection').style.display = 'none';
         resumeEditor.style.display = 'block';
     });
 });
 
-
+const resumeForm = document.getElementById('resume-form');
 resumeForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    userData.name = document.getElementById('name').value;
-    userData.email = document.getElementById('email').value;
-    
-    
-   
-    const selectedTemplate = document.querySelector('input[name="template"]:checked').value;
-    const generatedResumeContent = generateResume(selectedTemplate, userData);
- 
-    previewResume.innerHTML = generatedResumeContent;
-       
     generatedResume.style.display = 'block';
+    const previewResume = document.getElementById('preview-resume');
+    const resumeContent = generateResumeContent();
+    previewResume.innerHTML = resumeContent;
 });
 
+const downloadButton = document.getElementById('download-resume');
 downloadButton.addEventListener('click', () => {
-   
+    const resumeContent = generateResumeContent();
+    const blob = new Blob([resumeContent], { type: 'application/pdf' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'resume.pdf';
+    a.click();
+    URL.revokeObjectURL(url);
 });
-
-
-
-
-function generateResume(template, userData) {
-    console.log('Template selected:', template);
-    console.log('User data:', userData);
-    
-    let resumeContent = '';
-
-          
+function generateResumeContent() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const mobile = document.getElementById('mobile').value;
+    const gender = document.getElementById('gender').value;
+    const qualification = document.getElementById('qualification').value;
+    const college = document.getElementById('college').value;
+    const specialization= document.getElementById('specialization').value;
+    const gradYear = document.getElementById('gradYear').value;
+    const resumeContent = `
+    <p>Name: ${name}</p>
+    <p>Email: ${email}</p>
+    <p>Mobile Number: ${mobile}</p>
+    <p>Gender: ${gender}</p>
+    <p>Highest Qualification: ${qualification}</p>
+    <p>College: ${college}</p>
+    <p>Specialization/Branch: ${specialization}</p>
+    <p>Year of Graduation: ${gradYear}</p>
+    `;
+    return resumeContent;
 }
+
 
 
